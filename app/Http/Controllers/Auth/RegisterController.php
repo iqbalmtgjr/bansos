ed39226166f8f15abe941bbb78b6c6b34647aa81
Muhammad Iqbal <iqbalmtgjr@gmail.com>
2023-07-28 14:02:11 +0700

    ubah register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        if (empty(User::first())) {
+            return User::create([
+                'name' => $data['name'],
+                'username' => $data['username'],
+                'email' => $data['email'],
+                'role' => 0,
+                'password' => Hash::make($data['password']),
+            ]);
+        } else {
+            return redirect()->back();
+        }
     }
 }
